622e3a2a930d9e6f763bb8a49f3c1241e5046e2d
hoha_rejection <lim23@vcu.edu>
2020-11-27 11:37:20 -0500

    Create WeatherObservationStation7.sql

---
+++ 1BasicSelect/WeatherObservationStation7.sql
@@ -0,0 +1,6 @@
+/* Problem Link: https://www.hackerrank.com/challenges/weather-observation-station-7/problem?h_r=next-challenge&h_v=zen */
+
+SELECT DISTINCT CITY 
+FROM STATION
+/*  Last letter match a,e,i,o,u  */
+WHERE CITY RLIKE ".*[aeiou]$";
